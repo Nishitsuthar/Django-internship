fe995bb669c452d48c21e1c4a0a502ca105d5866
jon <nishitsuthar123@gmail.com>
2021-06-07 21:31:21 +0530

    form and just simple addition in form

--- djangointernship/myapp/urls.py
+++ djangointernship/myapp/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path("", views.homepageview, name="home"),
     path("about", views.aboutpageview, name="about"),
     path("contact", views.contactpageview, name="contact"),
+    path("form", views.formpageview, name="form"),
+    path("process", views.processpageview, name="process"),
+    path("signup", views.signuppageview, name="signup"),
+    path("Udata", views.userdataview, name="userdata"),
 ]
--- djangointernship/myapp/views.py
+++ djangointernship/myapp/views.py
@@ -3,15 +3,56 @@ from django.http import HttpResponse
 
 
 def homepageview(request):
-    return render(request,'home.html')
+    return render(request, "home.html")
 
 
 def aboutpageview(request):
-    return render(request,'about.html')
+    return render(request, "about.html")
 
 
 def contactpageview(request):
-    return render(request,'contact.html')
+    return render(request, "contact.html")
+
+
+def formpageview(request):
+    return render(request, "form.html")
+
+
+def processpageview(request):
+    print("welcome")
+    print(request.method)
+    print(request.POST)
+    a = int(request.POST["num1"])
+    b = int(request.POST["num2"])
+    c = a + b
+    print(c)
+    return render(request, "sum.html", {"mya": a, "myb": b, "mysum": c})
+
+
+def signuppageview(request):
+    return render(request, "signup.html")
+
+
+def userdataview(request):
+    print("user fulfilled form")
+    print(request.method)
+    print(request.POST)
+    fname = request.POST["first_name"]
+    lname = request.POST["last_name"]
+    email = request.POST["email"]
+    sub = request.POST["subject"]
+    mobile = int(request.POST["phone"])
+    return render(
+        request,
+        "userdata.html",
+        {
+            "my_fname": fname,
+            "my_lname": lname,
+            "myemail": email,
+            "sub": sub,
+            "my_mobile": mobile,
+        },
+    )
 
 
 # Create your views here.
